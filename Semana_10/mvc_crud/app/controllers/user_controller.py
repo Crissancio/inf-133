0cd77fd7ef436a9efc1b69ec9fe3f8a610fe7699
Crissancio <soyelpapu1509@gmail.com>
2024-05-13 12:23:14 -0400

    commit

--- Semana_10/mvc_crud/app/controllers/user_controller.py
+++ Semana_10/mvc_crud/app/controllers/user_controller.py
@@ -11,14 +11,14 @@ from models.user_model import User
 user_bp = Blueprint('user', __name__)
 
 
-@user_bp.route('/')
-def usuarios():
+@user_bp.route('/users')
+def list_users():
     users = User.get_all()
     return user_view.usuarios(users)
 
 # la ruta users esta asociada al registro de usuarios 
-@user_bp.route('/users', methods=['GET','POST'])
-def registro():
+@user_bp.route('/users/create', methods=['GET','POST'])
+def create_user():
     if request.method == 'POST':
         # obtenemos los datos del formulario
         name = request.form['name']
@@ -26,40 +26,48 @@ def registro():
         email = request.form['email']
         password = request.form['password']
         date = request.form['bday']
-        date = datetime.strptime(date, '%m/%d/%Y')
+        date = datetime.strptime(date, '%m-%d-%Y')
         # creamos un usuario y lo guardamos
         user = User(name, last, email, password, date)
         user.save()
         # redirigimos a la vista de usuarios
-        return redirect(url_for('user.usuarios'))
+        return redirect(url_for('user.list_users'))
     # llamamos a la vista de registro
     return user_view.registro()
 
-@user_bp.route('/users/<int:id>', methods=['GET'])
+'''@user_bp.route('/users/<int:id>', methods=['GET'])
 def obtener_usuario(id):
     user = User.get_by_id(id)
     if not user:
         return "Usuario no encontrado", 404
-    return user_view.actualizar(user)
+    return user_view.actualizar(user)'''
 
-@user_bp.route('/users/<int:id>', methods=['POST'])
-def actualizar(id):
+@user_bp.route('/users/<int:id>/update', methods=['GET','POST'])
+def update_user(id):
     user = User.get_by_id(id)
     if not user:
         return "Usuario no encontrado", 404
     
-    name = request.form['name']
-    last = request.form['last']
-    user.name = name
-    user.last_name = last
-    user.update()
-    
-    return redirect(url_for('user.usuarios'))
+    if request.method == 'POST':
+        name = request.form['name']
+        last = request.form['last']
+        email = request.form['email']
+        password = request.form['password']
+        bday = request.form['bday']
+        bday = datetime.strptime(bday, '%m-%d-%Y')
+        user.name = name
+        user.last_name = last
+        user.email = email
+        user.password = password
+        user.bday = bday
+        user.update()
+        return redirect(url_for('user.list_users'))
+    return user_view.actualizar(user)
 
-@user_bp.route('/user/<int:id>', methods=['GET'])
-def eliminar(id):
+@user_bp.route('/user/<int:id>/delete') #methods=['GET']
+def delete_user(id):
     user = User.get_by_id(id)
     if not user:
         return "Usuario no encontrado", 404
     User.delete(user)
-    return redirect(url_for('user.usuarios'))
+    return redirect(url_for('user.list_users'))
